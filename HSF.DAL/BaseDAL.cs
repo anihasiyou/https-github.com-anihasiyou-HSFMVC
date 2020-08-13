ad057c6c6279e7d4125c8592ba5f320582a30445
hc <hc@DESKTOP-TNFKMDG>
2020-08-13 13:51:27 +0800

    20200813

---
+++ HSF.DAL/BaseDAL.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using HSF.Models;
+using HSF.IDAL;
+using HSF.DBUtility;
+
+namespace HSF.DAL
+{
+    public class BaseDAL<T> : IBaseDAL<T> where T : class, new()
+    {
+        #region CRUD
+        /// <summary>
+        /// 根据主键返回实体
+        /// </summary>
+        public T GetById(int Id)
+        {
+            string sql = "";
+           return SqlDapperHelper<T>.QueryFirst()
+        }
+        /// <summary>
+        /// 新增
+        /// </summary>
+        public int Insert(T model)
+        {
+            using (var conn = SqlHelper.SqlConnection())
+            {
+                return conn.Insert<T>(model);
+            }
+        }
+        /// <summary>
+        /// 根据主键修改数据
+        /// </summary>
+        public int UpdateById(T model)
+        {
+            using (var conn = SqlHelper.SqlConnection())
+            {
+                return conn.UpdateById<T>(model);
+            }
+        }
+        /// <summary>
+        /// 根据主键修改数据 修改指定字段
+        /// </summary>
+        public int UpdateById(T model, string updateFields)
+        {
+            using (var conn = SqlHelper.SqlConnection())
+            {
+                return conn.UpdateById<T>(model, updateFields);
+            }
+        }
+        /// <summary>
+        /// 根据主键删除数据
+        /// </summary>
+        public int DeleteById(int Id)
+        {
+            using (var conn = SqlHelper.SqlConnection())
+            {
+                return conn.DeleteById<T>(Id);
+            }
+        }
+        /// <summary>
+        /// 根据主键批量删除数据
+        /// </summary>
+        public int DeleteByIds(object Ids)
+        {
+            using (var conn = SqlHelper.SqlConnection())
+            {
+                return conn.DeleteByIds<T>(Ids);
+            }
+        }
+        /// <summary>
+        /// 根据条件删除
+        /// </summary>
+        public int DeleteByWhere(string where)
+        {
+            using (var conn = SqlHelper.SqlConnection())
+            {
+                return conn.DeleteByWhere<T>(where);
+            }
+        }
+        #endregion
+        /// <summary>
+        /// 获取分页数据
+        /// </summary>
+        public IEnumerable<T> GetByPage(SearchFilter filter, out int total)
+        {
+            using (var conn = SqlHelper.SqlConnection())
+            {
+                return conn.GetByPage<T>(filter.pageIndex, filter.pageSize, out total, filter.returnFields, filter.where, filter.param, filter.orderBy, filter.transaction, filter.commandTimeout);
+            }
+        }
+        /// <summary>
+        /// 获取分页数据 联合查询
+        /// </summary>
+        public IEnumerable<T> GetByPageUnite(SearchFilter filter, out int total)
+        {
+            using (var conn = SqlHelper.SqlConnection())
+            {
+                return conn.GetByPageUnite<T>(filter.prefix, filter.pageIndex, filter.pageSize, out total, filter.returnFields, filter.where, filter.param, filter.orderBy, filter.transaction, filter.commandTimeout);
+            }
+        }
+        /// <summary>
+        /// 返回整张表数据
+        /// returnFields需要返回的列，用逗号隔开。默认null，返回所有列
+        /// </summary>
+        public IEnumerable<T> GetAll(string returnFields = null, string orderby = null)
+        {
+            using (var conn = SqlHelper.SqlConnection())
+            {
+                return conn.GetAll<T>(returnFields, orderby);
+            }
+        }
+        /// <summary>
+        /// 根据查询条件获取数据
+        /// </summary>
+        public IEnumerable<T> GetByWhere(string where = null, object param = null, string returnFields = null, string orderby = null)
+        {
+            using (var conn = SqlHelper.SqlConnection())
+            {
+                return conn.GetByWhere<T>(where, param, returnFields, orderby);
+            }
+        }
+    }
+}
---
+++ HSF.DBUtility/SqlHelper.cs
@@ -0,0 +1,234 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+using System.Configuration;
+
+namespace HSF.DBUtility
+{
+    public sealed class SQLHelper
+    {
+        //数据库连接字符串
+        private readonly static string ConnStr = ConfigurationManager.ConnectionStrings["connString"].ConnectionString;
+
+        /// <summary>
+        /// 返回所影响的条数
+        /// </summary> 
+        public static int ExecNonquery(string sql, SqlParameter[] parameter)
+        {
+            try
+            {
+                using (SqlConnection conn = new SqlConnection(ConnStr))
+                {
+                    conn.Open();
+                    using (SqlCommand cmd = conn.CreateCommand())
+                    {
+                        cmd.CommandText = sql;
+                        if (parameter != null)
+                        {
+                            cmd.Parameters.AddRange(parameter);
+                        }
+                        return cmd.ExecuteNonQuery();
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                //TODO：记录错误日志。略
+                return -1;
+            }
+        }
+
+
+        /// <summary>
+        /// 返回首行首列
+        /// </summary> 
+        public static object ExecScalar(string sql, SqlParameter[] parameter)
+        {
+            try
+            {
+                using (SqlConnection conn = new SqlConnection(ConnStr))
+                {
+                    conn.Open();
+                    using (SqlCommand cmd = conn.CreateCommand())
+                    {
+                        cmd.CommandText = sql;
+                        if (parameter != null)
+                        {
+                            cmd.Parameters.AddRange(parameter);
+                        }
+                        return cmd.ExecuteScalar();
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                //TODO：记录错误日志。略
+                return null;
+            }
+        }
+
+
+        /// <summary>
+        /// 返回结果集
+        /// </summary> 
+        public static DataSet ExecDataSet(string sql, SqlParameter[] parameter)
+        {
+            try
+            {
+                using (SqlConnection conn = new SqlConnection(ConnStr))
+                {
+                    conn.Open();
+                    using (SqlCommand cmd = conn.CreateCommand())
+                    {
+                        cmd.CommandText = sql;
+                        if (parameter != null)
+                        {
+                            cmd.Parameters.AddRange(parameter);
+                        }
+                        SqlDataAdapter adapter = new SqlDataAdapter(cmd);
+                        DataSet ds = new DataSet();
+                        adapter.Fill(ds);
+                        return ds;
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                //TODO：记录错误日志。略
+                return null;
+            }
+        }
+
+
+        /// <summary>
+        /// 返回一张表
+        /// </summary> 
+        public static DataTable ExecDataTable(string sql, SqlParameter[] parameter)
+        {
+            //try
+            //{
+            //    using (SqlConnection conn = new SqlConnection(ConnStr))
+            //    {
+            //        conn.Open();
+            //        using (SqlCommand cmd = conn.CreateCommand())
+            //        {
+            //            cmd.CommandText = sql;
+            //            if (parameter != null)
+            //            {
+            //                cmd.Parameters.AddRange(parameter);
+            //            }
+            //            SqlDataAdapter adapter = new SqlDataAdapter(cmd);
+            //            //DataSet ds = new DataSet();
+            //            //adapter.Fill(ds);
+            //            DataTable dt = new DataTable();
+            //            adapter.Fill(dt);
+            //            return dt;
+            //        }
+            //    }
+            //}
+            //catch (Exception e)
+            //{
+            //    //TODO：记录错误日志。略
+            //    return null;
+            //}
+
+            try
+            {
+                SqlDataAdapter da = new SqlDataAdapter(sql, ConnStr);
+                da.SelectCommand.Parameters.AddRange(parameter);
+                DataTable dt = new DataTable();
+                da.Fill(dt);
+                return dt;
+            }
+            catch (Exception e)
+            {
+                //TODO：记录错误日志。略
+                return null;
+            }
+        }
+
+        /// <summary>
+        /// 3.0 执行存储过程的方法
+        /// </summary>
+        /// <param name="procName">存储过程名</param>
+        /// <param name="ps">参数数组</param>
+        /// <returns></returns>
+        public static int ExcuteProc(string procName, params SqlParameter[] parameter)
+        {
+            try
+            {
+                using (SqlConnection conn = new SqlConnection(ConnStr))
+                {
+                    conn.Open();
+                    using (SqlCommand command = new SqlCommand(procName, conn))
+                    {
+                        command.CommandType = CommandType.StoredProcedure;
+                        if (parameter != null)
+                        {
+                            command.Parameters.AddRange(parameter);
+                        }
+                        return command.ExecuteNonQuery();
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                //TODO：记录错误日志。略
+                return -1;
+            }
+        }
+
+
+        /// <summary>
+        /// 分页存储过程
+        /// </summary>
+        /// <param name="TableName">表名</param>
+        /// <param name="FieldName">字段名</param>
+        /// <param name="wheres">where条件</param>
+        /// <param name="order">只能是desc or asc</param>
+        /// <param name="PageSize">每页条数</param>
+        /// <param name="PageIndex">当前页码</param>
+        /// <param name="TotalCount">总页码</param>
+        /// <param name="PageIdORField">指定字段来分页</param>
+        /// <param name="OrderField">排序指定的字段</param>
+        public static DataSet LinkProce(string TableName, string FieldName, string wheres, string order, string PageIdORField, string OrderField, ref int PageSize, ref int PageIndex)
+        {
+            using (SqlConnection conn = new SqlConnection(ConnStr))
+            {
+                conn.Open();
+                using (SqlCommand cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = "sp_PagingTabs";
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    cmd.Parameters.AddWithValue("@TableName", TableName);
+                    cmd.Parameters.AddWithValue("@FieldName", FieldName);
+                    cmd.Parameters.AddWithValue("@where", wheres);
+                    cmd.Parameters.AddWithValue("@Order", order);
+                    cmd.Parameters.AddWithValue("@OrderField", OrderField);
+                    cmd.Parameters.AddWithValue("@PageIdORField", PageIdORField);
+                    cmd.Parameters.AddWithValue("@PageSize", PageSize);
+                    cmd.Parameters.AddWithValue("@PageIndex", PageIndex);
+
+
+                    SqlDataAdapter da = new SqlDataAdapter();
+                    da.SelectCommand = cmd;
+                    DataSet ds = new DataSet();
+                    try
+                    {
+                        da.Fill(ds);
+                        return ds;
+                    }
+                    catch
+                    {
+                        return null;
+                    }
+                    finally
+                    {
+                        ds.Dispose();
+                        conn.Close();
+                    }
+                }
+            }
+        }
+    }
+}
