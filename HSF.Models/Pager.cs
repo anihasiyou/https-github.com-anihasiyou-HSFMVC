6314b2c385ffbcf1bd9ef60907350f9f8546549d
hc <hc@DESKTOP-TNFKMDG>
2020-08-13 16:08:12 +0800

    20200813

---
+++ HSF.Models/Pager.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace HSF.Models
+{
+    public class Pager
+    {
+        public static dynamic Paging(IEnumerable<dynamic> list, long total)
+        {
+            return new { code = 0, msg = "", count = total, data = list };
+        }
+    }
+}
